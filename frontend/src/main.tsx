fc1310440136a4d716bf18dd99054d2781b50a47
VitorSilami <vitor.amsilami12@gmail.com>
2026-07-18 11:58:12 -0300

    feat: substitui Bootstrap/Mazer por Tailwind CSS v4, semente de tokens do login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './styles/app.scss'
+import './app.css'
 import App from './App.tsx'
 import { ThemeProvider } from './features/theme/ThemeContext.tsx'
 
